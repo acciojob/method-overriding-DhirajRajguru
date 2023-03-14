be48be0e7010a9953edea7fda4e61d936db42fab
DhirajRajguru <drajguru9@gmail.com>
2023-03-14 19:24:04 +0530

    Done 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,7 +9,11 @@ public class Main
     public static void main(String[] args)
     {
         B obj = new B();
-        System.out.println(obj.meth());
+        obj.meth();
+
+        B obj2 = new B();
+        obj2.meth();
+
 
     }
 }
